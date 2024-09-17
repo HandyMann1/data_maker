c3b3ecf01a804f3c222754300f736578d0ee2feb
Artyom Arshinov <artyomarshinov@gmail.com>
2024-09-17 11:55:21 +0300

    added generate_passport_numbers

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ def generate_full_name(slavic_male_surnames: List[str], slavic_male_names: List[
     return full_name
 
 
+def generate_passport_numbers(region_numbers: List[str], used_passports_list: List[str]) -> str:
+    while True:
+        passport_number = random.choice(region_numbers) + str(random.randint(0, 99)) + random.randint(100000, 999999)
+        if passport_number not in used_passports_list:
+            return passport_number
