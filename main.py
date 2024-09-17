e5905c396edc369254b14f4c2cd6f4d1cbb1bb9d
Artyom Arshinov <artyomarshinov@gmail.com>
2024-09-17 21:55:40 +0300

    added basic generate_card_number

--- main.py
+++ main.py
@@ -24,3 +24,47 @@ def generate_passport_numbers(region_numbers: List[str], used_passports_list: Li
                                                                                                             999999)
         if passport_number not in used_passports_list:
             return passport_number
+
+
+def generate_card_number(used_cards, pay_system, bank) -> str:
+    if pay_system == 'Мир':
+        if bank == 'Сбербанк':
+            numbers = '2202'
+        elif bank == 'Тинькофф':
+            numbers = '2200'
+        elif bank == 'ВТБ':
+            numbers = '2204'
+        else:
+            numbers = '2206'
+    elif pay_system == 'MasterCard':
+        if bank == 'Сбербанк':
+            numbers = '5469'
+        elif bank == 'Тинькофф':
+            numbers = '5489'
+        elif bank == 'ВТБ':
+            numbers = '5443'
+        else:
+            numbers = '5406'
+    else:
+        if bank == 'Сбербанк':
+            numbers = '4276'
+        elif bank == 'Тинькофф':
+            numbers = '4277'
+        elif bank == 'ВТБ':
+            numbers = '4272'
+        else:
+            numbers = '4279'
+    while True:
+        rand_numbers = []
+        for i in range(3):
+            rand_numbers[i] = str(random.randint(1000, 9999))
+        card_number = numbers + ' ' + rand_numbers[0] + ' ' + rand_numbers[1] + ' ' + rand_numbers[2]
+        if card_number in used_cards:
+            if used_cards[card_number] < 5:
+                used_cards[card_number] += 1
+                break
+        else:
+            used_cards[card_number] = 1
+            break
+
+    return card_number
